494f75cdae8683c45e1b76e3887b5bfd7a2792dd
David Park <dysp92@gmail.com>
2017-12-21 13:13:10 -0800

    finished

--- spotify.js
+++ spotify.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const CLIENT_ID = 'eb6dfffda5534bb5996e872487be4321';
 
 const getFromApi = function (endpoint, query = {}) {
@@ -23,10 +25,42 @@ const getFromApi = function (endpoint, query = {}) {
 };
 
 let artist;
+// search?query=Sting&type=artist&market=US&offset=0&limit=20
+// artists/{id}/related-artists
+// artists/{id}/top-tracks
+// https://api.spotify.com/v1/artists/43ZHCT0cAZBISjO8DG9PnE/top-tracks?country=SE
+
 
 const getArtist = function (name) {
   // Edit me!
   // (Plan to call `getFromApi()` several times over the whole exercise from here!)
+  return getFromApi('search', {
+    query: name,
+    limit: 10,
+    type: 'artist',
+    market: 'US'
+  }).then(item => {
+    artist = item.artists.items[0];
+    console.log('artist:', artist);
+    return getFromApi(`artists/${artist.id}/related-artists`);
+  }).then(item => {
+    artist.related = item.artists;
+    console.log('related artists:', artist.related);
+    const arrayOfPromises = artist.related.map(item => {
+      return getFromApi(`artists/${item.id}/top-tracks`, {country: 'US'});
+    });
+    console.log(arrayOfPromises);
+    return Promise.all(arrayOfPromises);
+  }).then(results => {
+    console.log('results', results);
+    results.forEach((item, index) => {
+      artist.related[index].tracks = item.tracks;
+    });
+    console.log('artist', artist);
+    return artist;
+  }).catch(err => {
+    console.error('something wrong', err);
+  });
 };
 
 
